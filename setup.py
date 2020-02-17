0bd8fff846409d1990dbc2524231504569bcf741
722C <722C@users.noreply.github.com>
2020-02-17 11:18:41 -0700

    Update version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='lk-django-fcm',
-    version='0.0.2',
+    version='0.1.1',
     author='722C',
     description="LK's Django FCM Using Firebase Admin",
     license='MIT',
